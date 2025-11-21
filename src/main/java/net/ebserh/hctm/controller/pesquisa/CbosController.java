4a8ef1197823046596303b0f980cfdbb56fbe55e
Leonardo Victor Ribeiro <leonardovictor@outlook.com>
2025-11-21 16:02:55 -0300

    WIP #23: adiciona tela para listagem e dialog para cadastro/edição de CBO

--- src/main/java/net/ebserh/hctm/controller/pesquisa/CbosController.java
+++ src/main/java/net/ebserh/hctm/controller/pesquisa/CbosController.java
@@ -2,21 +2,57 @@ package net.ebserh.hctm.controller.pesquisa;
 
 import jakarta.faces.view.ViewScoped;
 import jakarta.inject.Named;
+import net.ebserh.hctm.model.pesquisa.Cbo;
+import net.ebserh.hctm.util.FacesUtils;
+import org.primefaces.PrimeFaces;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
 
 @Named
 @ViewScoped
 public class CbosController implements Serializable {
 
-    private String mensagem = "Controller de CBOs";
+    private static final Logger LOGGER = Logger.getAnonymousLogger();
+    private List<Cbo> cbos = new ArrayList<>();
+    private Cbo cbo;
 
-    public String getMensagem() {
-        return mensagem;
+    public void openDialogNova() {
+        cbo = new Cbo();
+        PrimeFaces.current().executeScript("PF('dialogCbo').show()");
     }
 
-    public void setMensagem(String mensagem) {
-        this.mensagem = mensagem;
+    public void editaCbo(Cbo cbo) {
+        if (cbo == null) {
+            FacesUtils.showError("É necessário selecionar um registro para edição.");
+            return;
+        }
+        this.cbo = cbo;
+        PrimeFaces.current().executeScript("PF('dialogCbo').show()");
     }
 
+    public void salva() {
+        LOGGER.severe("DBG Antes: " + cbos.size());
+        cbos.add(cbo);
+        LOGGER.severe("DBG Depois: " + cbos.size());
+        FacesUtils.showInfo("Dados salvos com sucesso!");
+    }
+
+    public List<Cbo> getCbos() {
+        return cbos;
+    }
+
+    public void setCbos(List<Cbo> cbos) {
+        this.cbos = cbos;
+    }
+
+    public Cbo getCbo() {
+        return cbo;
+    }
+
+    public void setCbo(Cbo cbo) {
+        this.cbo = cbo;
+    }
 }
