90ae8695bdff509fd9d46ad9f9d06c9ce8edcf65
Leonardo Victor Ribeiro <leonardovictor@outlook.com>
2025-12-09 15:30:27 -0300

    feat #39: configura conexão ao banco de dados no CbosService

--- src/main/java/net/ebserh/hctm/service/pesquisa/CbosService.java
+++ src/main/java/net/ebserh/hctm/service/pesquisa/CbosService.java
@@ -1,15 +1,15 @@
 package net.ebserh.hctm.service.pesquisa;
 
 import jakarta.ejb.Stateless;
-import jakarta.inject.Inject;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import net.ebserh.hctm.exception.CustomRuntimeException;
-import net.ebserh.hctm.model.pesquisa.BolsaProdutividadeCnpq;
 import net.ebserh.hctm.model.pesquisa.Cbo;
-import net.ebserh.hctm.util.MockDb;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 @Stateless
@@ -20,14 +20,33 @@ public class CbosService {
     @PersistenceContext
     private EntityManager entityManager;
 
-    @Inject
-    private MockDb mockDb;
 
     public List<Cbo> buscaCbos() {
-        return mockDb.getCbosCadastrados();
+        try {
+            return entityManager
+                    .createNamedQuery("Cbo.findAll", Cbo.class)
+                    .getResultList();
+        } catch (Exception e) {
+            LOGGER.log(Level.SEVERE, e.getMessage(), e);
+            throw new CustomRuntimeException("Ocorreu um erro ao buscar as CBOs");
+        }
     }
 
     public void salvaCbo(Cbo cbo) {
-        throw new CustomRuntimeException("Em construção...");
+        if (Objects.isNull(cbo))
+            throw new CustomRuntimeException("É necessário informar os dados da CBO.");
+
+        if (StringUtils.isBlank(cbo.getDescricao()))
+            throw new CustomRuntimeException("É necessário informar a descrição da CBO.");
+
+        try {
+            if (Objects.isNull(cbo.getId()))
+                entityManager.persist(cbo);
+            else
+                entityManager.merge(cbo);
+        } catch (Exception e) {
+            LOGGER.log(Level.SEVERE, e.getMessage(), e);
+            throw new CustomRuntimeException("Ocorreu um erro ao salvar os dados da CBO.");
+        }
     }
 }
